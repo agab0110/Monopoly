16ddd3133653026fb73dbd9f2a5d032fbdec4f3c
agab0110 <agabrielex01@gmail.com>
2022-10-24 15:44:17 +0200

    24/10/2022

--- src/gui/GameFramePanel.java
+++ src/gui/GameFramePanel.java
@@ -59,13 +59,12 @@ public class GameFramePanel extends JPanel{
 
         showButtons();
 
+        panel.setSize(720, 720);
         panel.setLayout(null);
         panel.setBackground(Color.BLACK);
         
         this.add(panel);
         panel.setVisible(true);
-
-        addAction(players.get(i));
 
         update();
     }
@@ -94,6 +93,76 @@ public class GameFramePanel extends JPanel{
         panel.add(turnOverButton);
 
         throwDice();
+
+        addAction(players.get(i));
+    }
+
+    private void showPrisonMenu() {
+        exitPrisonButton = new JButton("Esci di prigione gratutitamente");
+        payExitPrisonButton = new JButton("Esci di prigione pagando 50€");
+        turnOverButton = new JButton("Termina turno");
+        
+        exitPrisonButton.setBounds(540, 390, 200, 30);
+        payExitPrisonButton.setBounds(540, 430, 200, 30);
+        turnOverButton.setBounds(540, 470, 200, 30);
+
+        panel.add(exitPrisonButton);
+        panel.add(payExitPrisonButton);
+        panel.add(turnOverButton);
+
+        addPrisonAction(players.get(i));
+    }
+
+    private void showButtons() {
+        if(players.get(i).getStatus() == false) {
+            showMenu();
+        } else {
+            showPrisonMenu();
+        }
+    }
+
+    private void buyContract(Player player){
+        BuyContractFrame buyContractFrame = new BuyContractFrame(player, menager);
+        buyContractFrame.setVisible(true);   
+    }
+
+    private void update() {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {}
+
+                textField.setText("Turno di " + players.get(i).getName() + ", soldi: " + players.get(i).getMoney());
+                textArea.setText(setContracts());
+            }
+        });
+        thread.start();
+    }
+
+    private String setContracts() {
+        String contractName = "";
+
+        contracts = players.get(i).getContract();
+
+        for (Contract contract : contracts) {
+            contractName += contract.getName() + "\n";
+        }
+        return contractName;
+    }
+
+    private void payRent(Player player) {
+        PayRentFrame payRentFrame = new PayRentFrame(player, menager);
+        payRentFrame.setVisible(true);
+    }
+
+    private void throwDice(){
+        Random random = new Random();
+        JOptionPane.showMessageDialog(null,
+        random.nextInt(2,12),
+        "Dadi",
+        JOptionPane.INFORMATION_MESSAGE
+        );
     }
 
     private void addAction(Player player) {
@@ -166,7 +235,9 @@ public class GameFramePanel extends JPanel{
                 showButtons();
             }
         );
+    }
 
+    private void addPrisonAction(Player player) {
         exitPrisonButton.addActionListener(
             e -> {
                 player.setStatus(false);
@@ -192,71 +263,5 @@ public class GameFramePanel extends JPanel{
                 showButtons();               
             }
         );
-    }
-
-    private void buyContract(Player player){
-        BuyContractFrame buyContractFrame = new BuyContractFrame(player, menager);
-        buyContractFrame.setVisible(true);   
-    }
-
-    private void update() {
-        Thread thread = new Thread(() -> {
-            while (true) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {}
-
-                textField.setText("Turno di " + players.get(i).getName() + ", soldi: " + players.get(i).getMoney());
-                textArea.setText(setContracts());
-            }
-        });
-        thread.start();
-    }
-
-    private String setContracts() {
-        String contractName = "";
-
-        contracts = players.get(i).getContract();
-
-        for (Contract contract : contracts) {
-            contractName += contract.getName() + "\n";
-        }
-        return contractName;
-    }
-
-    private void payRent(Player player) {
-        PayRentFrame payRentFrame = new PayRentFrame(player, menager);
-        payRentFrame.setVisible(true);
-    }
-
-    private void throwDice(){
-        Random random = new Random();
-        JOptionPane.showMessageDialog(null,
-        random.nextInt(2,12),
-        "Dadi",
-        JOptionPane.INFORMATION_MESSAGE
-        );
-    }
-
-    private void showPrisonMenu() {
-        exitPrisonButton = new JButton("Esci di prigione gratutitamente");
-        payExitPrisonButton = new JButton("Esci di prigione pagando 50€");
-        turnOverButton = new JButton("Termina turno");
-        
-        exitPrisonButton.setBounds(540, 390, 200, 30);
-        payExitPrisonButton.setBounds(540, 430, 200, 30);
-        turnOverButton.setBounds(540, 470, 200, 30);
-
-        panel.add(exitPrisonButton);
-        panel.add(payExitPrisonButton);
-        panel.add(turnOverButton);
-    }
-
-    private void showButtons() {
-        if(players.get(i).getStatus() == false) {
-            showMenu();
-        } else {
-            showPrisonMenu();
-        }
     }
 }
